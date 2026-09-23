a8a4a509da2c7d5a6af5e96266ecd095e121bc95
Andrej0172 <345073@student.mboutrecht.nl>
2026-09-23 13:46:27 +0200

    feat: add seller overview view shell with page header

---
+++ app/Views/verkopers/index.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Verkopersoverzicht: alle shops en privéverkopers met een stand.
+ *
+ * Happy flow  : de kaarten van alle verkopers die aan een actief evenement
+ *               gekoppeld zijn.
+ * Unhappy flow: melding wanneer er geen verkopers zijn of de database faalt.
+ *
+ * @var string $titel
+ * @var string $pad
+ */
+?>
+<section class="pagina-kop">
+    <div class="container">
+        <p class="pagina-kop__label">Verkopers</p>
+        <h1 class="pagina-kop__titel">Wie staat er op de vloer?</h1>
+        <p class="pagina-kop__tekst">
+            Een overzicht van alle shops en privéverkopers met een stand op de
+            komende editie: van sneakerstores tot customizers en streetfood.
+        </p>
+    </div>
+</section>
+
+<section class="sectie">
+    <div class="container">
+        <!-- Wordt in de volgende stap gevuld met de verkoperskaarten. -->
+    </div>
+</section>
